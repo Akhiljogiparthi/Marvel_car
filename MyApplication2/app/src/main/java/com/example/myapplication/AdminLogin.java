1b02e2159045729fb1332aa0276d440b584ee548
Akhiljogiparthi <akhiljogiparthi96@gmail.com>
2019-10-19 15:30:06 -0400

    Admin login with default values created

--- MyApplication2/app/src/main/java/com/example/myapplication/AdminLogin.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/AdminLogin.java
@@ -2,7 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AdminLogin extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class AdminLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+        Button adlogin = (Button)findViewById(R.id.adlogin);
+        Button adminfp = (Button) findViewById(R.id.adminfp);
+        TextView adminlogin = (TextView) findViewById(R.id.adlogin);
+        final EditText username = (EditText) findViewById(R.id.username);
+        final EditText adpwd = (EditText) findViewById(R.id.adpwd);
+        adlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ("admin".equals(username.getText().toString()) && "password".equals(adpwd.getText().toString())) {
+                    Intent i = new Intent(AdminLogin.this, userhome.class);
+                    startActivity(i);
+                }
+            }
+
+        });
     }
 }
