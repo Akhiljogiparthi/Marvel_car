a8dc1150b7116ddb97270977107c164e57f93012
vedantherukulla <vedanth19@gmail.com>
2019-10-20 11:16:43 -0400

    User home options created

--- MyApplication2/app/src/main/java/com/example/myapplication/background.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/background.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SignUp extends AppCompatActivity {
+public class background extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-
+        setContentView(R.layout.activity_background);
     }
 }
