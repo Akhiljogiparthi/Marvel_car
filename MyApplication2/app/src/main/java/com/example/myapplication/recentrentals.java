bd07853a413c9ad2b6b64868f89eb5092a35e2f1
Akhiljogiparthi <akhiljogiparthi96@gmail.com>
2019-10-20 21:13:10 -0400

    user home options connectivity created

--- MyApplication2/app/src/main/java/com/example/myapplication/recentrentals.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/recentrentals.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class background extends AppCompatActivity {
+public class recentrentals extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_background);
+        setContentView(R.layout.activity_recentrentals);
     }
 }
