39dc57e49cd509fd65b106793f24ee86de868e9c
vedantherukulla <vedanth19@gmail.com>
2019-10-26 15:04:22 -0400

    admin can create vehicle added

--- app/src/main/java/com/example/marvel_cars/createvehicle.java
+++ app/src/main/java/com/example/marvel_cars/createvehicle.java
@@ -2,7 +2,11 @@ package com.example.marvel_cars;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class createvehicle extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class createvehicle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createvehicle);
+        Button vecsubmit=(Button)findViewById(R.id.vecsubmit);
+        final TextView view1 = (TextView)findViewById(R.id.vectext);
+        vecsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               view1.setText("New vehicle has been created , go back to home page");
+            }
+        });
     }
 }
